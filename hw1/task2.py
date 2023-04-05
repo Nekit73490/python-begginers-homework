6ac12b5abd02b2bc650cfc91e5c9ddf1ddef6a9d
Nekit73490 <nekit73490@gmail.com>
2023-04-05 12:32:13 +0400

    finished task2

--- hw1/task2.py
+++ hw1/task2.py
@@ -1,7 +1,9 @@
 
-x = 0
-y = 1
-z = 1
 
-print((not(x or y or z)) == ((not x) and (not y) and( not z)))
-
+for x in range(0,2):
+    # print(x)
+    for y in range(0,2):
+        # print(y)
+        for z in range(0,2):
+            # print(z)
+            print((not(x or y or z)) == ((not x) and (not y) and( not z)))
