84163fd7bdbbcac648b22375f8ae52cf0d10ae92
Nekit73490 <nekit73490@gmail.com>
2023-05-04 10:26:04 +0400

    Update task3.py

--- hw3/task3.py
+++ hw3/task3.py
@@ -4,20 +4,23 @@ working_list = working_list.split(',')
 
 count = 0
 
+def scaling(curent_number, starting_pos):
+  return((float(curent_number[starting_pos:])/(10**len(curent_number[starting_pos:]))))
+
 for i in working_list:
     if "." in i :
         if count < 1 :
             start = i.find(".") + 1
-            our_min = (float(i[start:])/(10**len(i[start:])))
-            our_max = (float(i[start:])/(10**len(i[start:])))
+            our_min = scaling(i,start)
+            our_max = scaling(i,start)
             # print(our_max, ' ', our_min)
             count = 1
         else :
             start = i.find(".") + 1
-            if (float(i[start:])/(10**len(i[start:]))) < our_min:
-                our_min = (float(i[start:])/(10**len(i[start:])))
-            if (float(i[start:])/(10**len(i[start:]))) > our_max: 
-                our_max = (float(i[start:])/(10**len(i[start:])))
+            if scaling(i,start) < our_min:
+                our_min = scaling(i,start)
+            if scaling(i,start) > our_max: 
+                our_max = scaling(i,start)
             # print(our_max, ' ', our_min)
 
 print(our_max - our_min)
